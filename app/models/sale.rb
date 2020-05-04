e32898ac2a7376b5dc61967485a84cfbf149dd63
shcyiza <patick.cyiza@gmail.com>
2020-05-04 22:10:46 +0200

    method to determine sales percentage with index instead cyclic methods

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,24 +3,20 @@
 class Sale < ApplicationRecord
   belongs_to :saleable, polymorphic: true
 
+  def self.royalty_rate
+    {
+      'Download Albums' => 0.30,
+      'Subscription Audio Streams' => 0.50,
+      'Ad-Supported Audio Streams' => 0.50,
+      'Non-interactive Radio' => 0.50,
+      'Cloud Match Units' => 0.50,
+      'Mid-Tier Subscription Audio Streams' => 0.50,
+      'Physical Sales' => 0.15,
+      'Download Tracks' => 0.30
+    }
+  end
+
   def sale_rate
-    case transaction_type
-    when 'Download Albums'
-      0.30
-    when 'Subscription Audio Streams'
-      0.50
-    when 'Ad-Supported Audio Streams'
-      0.50
-    when 'Non-interactive Radio'
-      0.50
-    when 'Cloud Match Units'
-      0.50
-    when 'Mid-Tier Subscription Audio Streams'
-      0.50
-    when 'Physical Sales'
-      0.15
-    when 'Download Tracks'
-      0.30
-    end
+    royalty_rate[transaction_type]
   end
 end
