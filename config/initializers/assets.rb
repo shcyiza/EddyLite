c5facfeefadac94765a43c1276b6521739761d0e
shcyiza <patick.cyiza@gmail.com>
2020-05-22 21:25:15 +0200

    css problems?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( bulma.min.css )
 # Precompile additional assets.
-# application.js, application.sass, and all non-JS/CSS in the app/assets
+# application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
